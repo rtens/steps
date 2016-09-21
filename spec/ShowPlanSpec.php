0e8b010ca09a2da3c4de02b36d3bd8b3da2ec52d
Nikolas Martens <Nikolas.M@rtens.org>
2016-09-21 20:00:26 +0200

    Don't show finished blocks

--- spec/ShowPlanSpec.php
+++ spec/ShowPlanSpec.php
@@ -1,6 +1,7 @@
 <?php namespace spec\rtens\steps;
 
-use rtens\steps\app\Application;;
+use rtens\steps\app\Application;
+use rtens\steps\events\BlockFinished;
 use rtens\steps\events\BlockPlanned;
 use rtens\steps\model\Block;
 use rtens\steps\model\BlockIdentifier;
@@ -9,6 +10,8 @@ use rtens\steps\model\Plan;
 use rtens\steps\model\Time;
 use rtens\steps\ShowPlan;
 use watoki\karma\testing\Specification;
+
+;
 
 class ShowPlanSpec extends Specification {
 
@@ -27,6 +30,19 @@ class ShowPlanSpec extends Specification {
                     1
                 )
             ];
+        });
+    }
+
+    public function finishedBlock() {
+        $this->given(new BlockPlanned(new BlockIdentifier('fooBlock'), new GoalIdentifier('foo'), 1, Time::now()));
+        $this->given(new BlockPlanned(new BlockIdentifier('barBlock'), new GoalIdentifier('bar'), 1, Time::now()));
+        $this->given(new BlockFinished(new BlockIdentifier('fooBlock'), Time::now()));
+        $this->when(new ShowPlan());
+
+        $this->then->returnShouldMatch(function (Plan $plan) {
+            return
+                count($plan->getBlocks()) == 1
+                && $plan->getBlocks()[0]->getBlock() == new BlockIdentifier('barBlock');
         });
     }
 }
--- src/model/Plan.php
+++ src/model/Plan.php
@@ -1,19 +1,30 @@
 <?php namespace rtens\steps\model;
+use rtens\steps\events\BlockFinished;
 use rtens\steps\events\BlockPlanned;
 
 class Plan {
 
+    /**
+     * @var Block[]
+     */
     private $blocks = [];
 
+    /**
+     * @return Block[]
+     */
     public function getBlocks() {
-        return $this->blocks;
+        return array_values($this->blocks);
     }
 
     public function applyBlockPlanned(BlockPlanned $e) {
-        $this->blocks[] = new Block(
+        $this->blocks[(string)$e->getBlock()] = new Block(
             $e->getBlock(),
             $e->getGoal(),
             $e->getUnits()
         );
+    }
+
+    public function applyBlockFinished(BlockFinished $e) {
+        unset($this->blocks[(string)$e->getBlock()]);
     }
 }
