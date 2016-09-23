d2339857442f8518c50149312e4d9bd63e9d4260
Nikolas Martens <Nikolas.M@rtens.org>
2016-09-23 09:41:39 +0200

    Register Queries as non-modifying

--- src/app/Application.php
+++ src/app/Application.php
@@ -1,10 +1,11 @@
 <?php namespace rtens\steps\app;
 
 use rtens\domin\delivery\web\WebApplication;
-use rtens\domin\reflection\ObjectActionGenerator;
+use rtens\domin\reflection\GenericObjectAction;
 use rtens\steps\model\Identifier;
 use rtens\steps\model\Time;
 use watoki\karma\implementations\commandQuery\CommandQueryApplication;
+use watoki\karma\implementations\commandQuery\Query;
 use watoki\karma\stores\EventStore;
 
 class Application extends CommandQueryApplication {
@@ -20,6 +21,33 @@ class Application extends CommandQueryApplication {
 
     public function run(WebApplication $curir) {
         $curir->setNameAndBrand('steps');
-        (new ObjectActionGenerator($curir->actions, $curir->types, $curir->parser))->fromFolder(__DIR__ . '/..', [$this, 'handle']);
+        $this->registerActions($curir);
+    }
+
+    private function registerActions(WebApplication $curir) {
+        foreach ($this->findActionsIn(__DIR__ . '/../*.php') as $class) {
+            $curir->actions->add($this->makeActionId($class),
+                $this->makeAction($curir, $class)->generic()
+                    ->setModifying(!is_subclass_of($class, Query::class)));
+        }
+    }
+
+    private function findActionsIn($folder) {
+        $before = get_declared_classes();
+
+        foreach (glob($folder) as $file) {
+            include_once($file);
+        }
+
+        $newClasses = array_diff(get_declared_classes(), $before);
+        return $newClasses;
+    }
+
+    private function makeActionId($class) {
+        return lcfirst((new \ReflectionClass($class))->getShortName());
+    }
+
+    private function makeAction(WebApplication $curir, $class) {
+        return (new GenericObjectAction($class, $curir->types, $curir->parser, [$this, 'handle']));
     }
 }
