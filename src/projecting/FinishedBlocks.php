c1cacd26f54b34444aa71b3f08d64101979769e3
Nikolas Martens <Nikolas.M@rtens.org>
2016-09-28 15:41:04 +0200

    Filter finished blocks by time

--- src/projecting/FinishedBlocks.php
+++ src/projecting/FinishedBlocks.php
@@ -1,13 +1,33 @@
 <?php namespace rtens\steps\projecting;
 
 class FinishedBlocks extends BlockList {
+    /**
+     * @var null|\DateTime
+     */
+    private $after;
+    /**
+     * @var null|\DateTime
+     */
+    private $before;
+
+    /**
+     * @param \DateTime|null $after
+     * @param \DateTime|null $before
+     */
+    public function __construct(\DateTime $after = null, \DateTime $before = null) {
+        $this->after = $after;
+        $this->before = $before;
+    }
 
     /**
      * @return Block[]
      */
     public function getBlocks() {
         return array_values(array_filter(parent::getBlocks(), function (Block $block) {
-            return $block->getIsFinished();
+            return
+                $block->getIsFinished()
+                && (!$this->after || $block->getStarted() > $this->after)
+                && (!$this->before || $block->getFinished() < $this->before);
         }));
     }
 
