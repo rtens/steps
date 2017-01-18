0201962c24740273671892356faf33173419eff0
Nikolas Martens <Nikolas.M@rtens.org>
2017-01-18 15:55:27 +0100

    Added menu items

--- src/app/Application.php
+++ src/app/Application.php
@@ -1,6 +1,7 @@
 <?php
 namespace rtens\steps2\app;
 
+use rtens\domin\delivery\web\menu\ActionMenuItem;
 use rtens\domin\delivery\web\WebApplication;
 use rtens\udity\app\Application as Udity;
 
@@ -9,6 +10,10 @@ class Application extends Udity {
     public function run(WebApplication $ui, array $domainClasses) {
         $ui->setNameAndBrand('steps2');
         $ui->renderers->add(new CurrentStepRenderer($ui->renderers));
+
+        $ui->menu->add(new ActionMenuItem('New', 'Goal$create'));
+        $ui->menu->add(new ActionMenuItem('Goals', 'GoalList'));
+        $ui->menu->add(new ActionMenuItem('Walk', 'Walk'));
 
         parent::run($ui, $domainClasses);
     }
