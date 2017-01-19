a042c00cddd8dfa859887698f2064b1566b0c9f7
Nikolas Martens <Nikolas.M@rtens.org>
2017-01-19 12:24:14 +0100

    Use CachingEventStore

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ use rtens\domin\delivery\web\adapters\curir\root\IndexResource;
 use rtens\domin\delivery\web\WebApplication;
 use rtens\steps2\app\Application;
 use watoki\curir\WebDelivery;
+use watoki\karma\stores\CachingEventStore;
 use watoki\karma\stores\StoringEventStore;
 use watoki\stores\stores\FileStore;
 
 WebDelivery::quickResponse(IndexResource::class,
     WebDelivery::init(null,
         WebApplication::init(function (WebApplication $app) {
-            (new Application(new StoringEventStore(new FileStore('user/events'))))
+            (new Application(new CachingEventStore(new StoringEventStore(new FileStore('user/events')))))
                 ->run($app, Application::loadClasses('src/domain'));
         })));
